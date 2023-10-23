50bbe273f9ca1717bb89b4d0c1c6bdc6fa66d076
dhrianko <d.hrianko@geniusee.com>
2023-10-23 12:57:13 +0300

    update

--- telegram_stats_bot/main.py
+++ telegram_stats_bot/main.py
@@ -219,12 +219,16 @@ if __name__ == '__main__':
     store = PostgresStore(args.postgres_url)
     stats = StatsRunner(store.engine, tz=args.tz)
 
-    admin_ids = [334377723, 356296256]    
-    if args.admin_id != 0:
-        # filters = ~Filters.update.edited_message & Filters.user(user_id=args.admin_id) & Filters.chat_type.private
-        filters = ~Filters.update.edited_message & Filters.user(admin_ids) & Filters.chat_type.private
-    else:
-        filters = ~Filters.update.edited_message & Filters.chat_type.private
+    admin_ids = [334377723, 356296256]
+    print(admin_ids)    
+    amin_filter = Filters.user()
+    amin_filter.add_user_ids(admin_ids)
+    # if args.admin_id != 0:
+    #     # filters = ~Filters.update.edited_message & Filters.user(user_id=args.admin_id) & Filters.chat_type.private
+    #     filters = ~Filters.update.edited_message & Filters.user(admin_ids) & Filters.chat_type.private
+    # else:
+    #     filters = ~Filters.update.edited_message & Filters.chat_type.private
+    filters = ~Filters.update.edited_message & amin_filter & Filters.chat_type.private
 
     stats_handler = CommandHandler('stats', print_stats, filters=filters, run_async=True)
     dispatcher.add_handler(stats_handler)
